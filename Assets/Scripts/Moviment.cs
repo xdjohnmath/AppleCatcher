94294041c827ac4af15e9575f5a56ed3691f341e
PC-JOÃO\João <xdjohnmath@gmail.com>
2016-11-24 01:07:22 -0300

    Particle System

--- Assets/Scripts/Moviment.cs
+++ Assets/Scripts/Moviment.cs
@@ -3,9 +3,9 @@ using System.Collections;
 
 public class Moviment : MonoBehaviour {
 
-	public float vel;
+	public float vel; 
 
-	public ParticleSystem part;
+	public ParticleSystem part, folhenhas;
 
 	void Start () {
 	
@@ -22,14 +22,20 @@ public class Moviment : MonoBehaviour {
 		transform.Translate	(0, 0, translation);
 		transform.Rotate	(0, rotation, 0);
 
+	// Para as folhenhas suberem           //BUG
+		if (Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.DownArrow) || Input.GetKeyDown (KeyCode.LeftArrow) || Input.GetKeyDown (KeyCode.RightArrow)) {
+			folhenhas.Play ();
+		} else if (Input.GetKeyUp (KeyCode.UpArrow) || Input.GetKeyUp (KeyCode.DownArrow) || Input.GetKeyUp (KeyCode.LeftArrow) || Input.GetKeyUp (KeyCode.RightArrow)) {
+			folhenhas.Stop ();
+		}
 	}
+
 
 	void OnTriggerEnter (Collider c) {
 
 		if (c.name == "Apple"){
 			part.Play ();
-		
-		}	
-
+		}
+			
 	}
 }
